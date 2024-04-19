c7386b298a161324163ecacf70f2e90612906e07
stmmarvin <marvinakpabot@gmail.com>
2024-04-19 14:29:34 +0200

    navbar komt nu overeen bij de andere pagina's

--- contact.php
+++ contact.php
@@ -5,24 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
+    <nav>
+        <div id="navbar">
+            <h1 id="banner_text">MBO Utrecht</h1>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Course.php">Opleiding</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="#">FAQ</a></li> <!-- Daar gaan we voor sprint 2 aan het werk-->
+            </ul>
+        </div>
 </head>
 <body>
-    <!--navbar-->
-    <header>
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Artikelen</a></li>
-                <li><a href="">Info</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="">FAQ</a></li>
-            </ul>
-        </nav>
-
-        <div id="banner">
-            <h1 id="banner_text">MBO Utrecht</h1>
-        </div>
-    </header>
+    
 
     <main>
         <div id="arrow_container">
