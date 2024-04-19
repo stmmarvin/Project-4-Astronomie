e99769153fd7ba239c04281a5d084d2f73d3e10e
stmmarvin <marvinakpabot@gmail.com>
2024-04-19 14:22:44 +0200

    navbar index en css aangepast

--- Course.php
+++ Course.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <nav>
+        <div id="banner">
+        <h1 id="banner_text">MBO Utrecht</h1>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="Course.php">Opleiding</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#">FAQ</a></li> <!-- Daar gaan we voor sprint 2 aan het werk-->
+        </ul>
+    </div>
+    </nav>
 </head>
 <body>
     
