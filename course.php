80591ca3ba17a9471bacfff7f40a46733bfbe782
posadist-dolphin <lucavandoorn@hotmail.com>
2024-04-22 10:30:41 +0200

    stuff

--- course.php
+++ course.php
@@ -5,19 +5,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
-    <nav>
-        <div id="banner">
-        <h1 id="banner_text">MBO Utrecht</h1>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="Course.php">Opleiding</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="#">FAQ</a></li> <!-- Daar gaan we voor sprint 2 aan het werk-->
-        </ul>
-    </div>
-    </nav>
 </head>
 <body>
-    
+<header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Artikelen</a></li>
+                <li><a href="">Info</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+        </nav>
+        <div id="banner">
+            <h1 id="banner_text">MBO Utrecht</h1>
+        </div>
+    </header>
+    <main>
+
+        <div class="divider"></div>
+
+        <section class="content">
+            <img src="" alt="" class="top">
+            <img src="" alt="" class="bottom">
+            <h3 class="title"></h3>
+            <p></p>
+            <a href="" class="social-media-mini"><img src="" alt=""></a>
+            <a href="" class="social-media-mini"><img src="" alt=""></a>
+            <a href="" class="social-media-mini"><img src="" alt=""></a>
+            <p></p>
+        </section>
+
+        <div class="divider"></div>
+
+        <video src="" class="vid-small"></video>
+        <video src="" class="vid-small"></video>
+
+        <div class="divider"></div>
+
+        <div class="img-row-container">
+            <img src="" alt="" class="tall-img">
+            <img src="" alt="" class="tall-img">
+            <img src="" alt="" class="tall-img">
+            <img src="" alt="" class="tall-img">
+        </div>
+    </main>
+    <footer>
+        <div class="contact-info">
+            <p class="adres">Australiëlaan 25, Utrecht, 3526AB</p>
+            <p class="email">astronomie@utrecht.nl</p>
+        </div>
+        <div class="social-media-links">
+            <a href="" class="social-media-link" id="twitter">
+                <img src="" alt="">
+            </a>
+            <a href="" class="social-media-link" id="facebook">
+                <img src="" alt="">
+            </a>
+            <a href="" class="social-media-link" id="youtube">
+                <img src="" alt="">
+            </a>
+        </div>
+    </footer>
 </body>
 </html>
