a9534b9d3050536f6dec4fad3e73e4b41e2065d0
posadist-dolphin <lucavandoorn@hotmail.com>
2024-04-22 13:02:48 +0200

    updated pre-pre-alpha

--- scripts/game.js
+++ scripts/game.js
@@ -33,7 +33,34 @@ class Ship {
     }
 }
 
-cargo_names_default = ["Space Whale","Earth or Bust!","Bucket","Century Hawk","Rusty","Primrose","Old Man","Gold Digger","Walter",""];
+class Mod {
+    constructor(name, type, slots_used) {
+        this.name = name;
+        this.type = type;
+        this.slots_used = slots_used;
+    }
+}
+
+class Weapon extends Mod {
+    constructor(name, type, slots_used, damage, fire_rate, ammo) {
+        super.constructor(name,type,slots_used);
+        this.damage = damage;
+        this.fire_rate = fire_rate;
+        this.ammo = ammo;
+    }
+}
+
+class Utility extends Mod {
+    constructor(name, type, slots_used, effect, strength) {
+        super(name,type,slots_used);
+        this.effect = effect;
+        this.strength = strength;
+    }
+}
+
+
+
+cargo_names_default = ["Space Whale","Earth or Bust!","Bucket","Century Hawk","Rusty","Primrose","Old Man","Gold Digger","Walter","Red Dwarf"];
 cargo_names_pirate = ["Haula","Money printer","Iron Giant","Last Train Home","Going Merry"];
 
 war_names_default = ["Blitzer","Brittannia","Dreadnought","Samurai","TT Far From Home","Paragon","Grute Pier"];
@@ -42,6 +69,9 @@ war_names_pirate = ["Dreadwell","Brittannia","Hound","Ronin","Hunter","Dirty Dog
 dingus = new Ship("aa",12,500,500,5,3,1,2,0.5,0.5,100,100,100);
 bingus = Ship.create_cargo();
 
-console.log(bingus)
+gungus = new Utility("External cargo hold", "u", "1", "increase_cargo","100");
+
+console.log(bingus);
+console.log(gungus);
 
 
