856226d67d093d17b413f6095d804cc30f313ad1
estellemsastro <estelle2007-sastropawiro@outlook.com>
2024-04-22 13:20:42 +0100

    info van shuhd zit er in bij de home

---
+++ contact.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/contact.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Artikelen</a></li>
+                <li><a href="">Info</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+        </nav>
+        <div id="banner">
+            <h1 id="banner_text">MBO Utrecht</h1>
+        </div>
+    </header>
+    <main>
+        <div id="arrow_container">
+            <div class="arrow" id="arrow_left"></div>
+            <img src="" alt="" class="arrow_img">
+            <img src="" alt="" class="arrow_img hidden">
+            <img src="" alt="" class="arrow_img hidden">
+            <img src="" alt="" class="arrow_img hidden">
+            <div class="arrow" id="arrow_right"></div>
+        </div>
+
+        <div class="divider"></div>
+
+        <div class="contacts_container">
+            <div class="contact">
+                <img src="" alt="">
+                <div class="contact_info_box">
+                    <p></p>
+                </div>
+                <button class="contact_button"></button>
+            </div>
+            <div class="contact">
+                <img src="" alt="">
+                <div class="contact_info_box">
+                    <p></p>
+                </div>
+                <button class="contact_button"></button>
+            </div>
+            <div class="contact">
+                <img src="" alt="">
+                <div class="contact_info_box">
+                    <p></p>
+                </div>
+                <button class="contact_button"></button>
+            </div>
+        </div>
+
+        <form action="post" id="contact_form">
+            <input type="email" name="" id="">
+            <textarea name="" id="" cols="30" rows="10">
+
+            </textarea><input type="button" name="" id="">
+        </form>
+    </main>
+    <footer>
+        <div class="contact-info">
+            <p class="adres">Australiëlaan 25, Utrecht, 3526AB</p>
+            <p class="email">astronomie@utrecht.nl</p>
+        </div>
+        <div class="social-media-links">
+            <a href="" class="social-media-link" id="twitter">
+                <img src="media/img/Logo_of_Twitter_wikimedia_commons.svg" alt="">
+            </a>
+            <a href="" class="social-media-link" id="facebook">
+                <img src="media/img/2021_Facebook_icon_wikimedia_commons.svg" alt="">
+            </a>
+            <a href="media/img/YouTube_full-color_icon_(2017)_wikimedia_commons.svg" class="social-media-link" id="youtube">
+                <img src="" alt="">
+            </a>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ course.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Artikelen</a></li>
+                <li><a href="">Info</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+        </nav>
+        <div id="banner">
+            <h1 id="banner_text">MBO Utrecht</h1>
+        </div>
+    </header>
+    <main>
+        <section class="opleiding_info">
+            <h2>Welkom bij de Opleiding Astronomie</h2>
+            <p>
+                Astronomie is de wetenschap die zich bezighoudt met de bestudering van hemellichamen zoals sterren, planeten, kometen, en meer. In onze opleiding Astronomie leer je alles over de mysteries van het universum en ontwikkel je vaardigheden om de kosmos te verkennen.
+            </p>
+            <h3>Wat kun je verwachten?</h3>
+            <p>
+                Tijdens de opleiding Astronomie kun je verwachten:
+                <ul>
+                    <li>Diepgaande kennis van astronomische concepten</li>
+                    <li>Praktische ervaring met observaties en onderzoek</li>
+                    <li>Samenwerking met professionals in het veld</li>
+                    <li>Mogelijkheden voor stages en internationale uitwisselingsprogramma's</li>
+                </ul>
+            </p>
+        </section>
+
+        <section class="opleiding_details">
+            <h2>Opleidingsdetails</h2>
+            <ul>
+                <li><strong>Kosten:</strong> Neem contact op met onze administratie voor informatie over collegegeld en eventuele studiefinanciering.</li>
+                <li><strong>Duur:</strong> De opleiding Astronomie duurt normaal gesproken vier jaar bij een voltijdse studie.</li>
+                <li><strong>Laptop eisen:</strong> Voor de opleiding Astronomie moet je een laptop hebben, omdat we de lessen volgen van uit een onlineonderwijsysteem</li>
+                <li><strong>BBL/BOL:</strong> Deze opleiding wordt aangeboden als voltijdse BOL (BeroepsOpleidende Leerweg).</li>
+                <li><strong>Open dagen:</strong> Bekijk onze website voor de data van de eerstvolgende open dagen.</li>
+            </ul>
+        </section>
+    </main>
+    
+    <footer>
+        <div class="contact-info">
+            <p class="adres">Australiëlaan 25, Utrecht, 3526AB</p>
+            <p class="email">astronomie@utrecht.nl</p>
+        </div>
+        <div class="social-media-links">
+            <a href="" class="social-media-link" id="twitter">
+                <img src="media/img/Logo_of_Twitter_wikimedia_commons.svg" alt="">
+            </a>
+            <a href="" class="social-media-link" id="facebook">
+                <img src="media/img/2021_Facebook_icon_wikimedia_commons.svg" alt="">
+            </a>
+            <a href="media/img/YouTube_full-color_icon_(2017)_wikimedia_commons.svg" class="social-media-link" id="youtube">
+                <img src="" alt="">
+            </a>
+        </div>
+    </footer>
+</body>
+</html>
