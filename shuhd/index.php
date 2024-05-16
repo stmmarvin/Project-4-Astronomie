e21c12b786b468922c927d9e34bc848d46904193
Shuhd <shuhdalazzani27@gmail.com>
2024-05-16 09:47:58 +0200

    ik ga hier werken dan kunnen jullie de nieuwe dingen die ik heb toegeovoegd in jullie mappen zetten

---
+++ shuhd/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Astronomie</title>
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="scss/_base.scss" />
+  
+  
+</head>
+
+<body>
+  <header>
+    <nav>
+      <div class="container row jc-center">
+        <div class="main-menu">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="Opleiding/opleiding.php">Opleiding</a></li>
+            <!--<li><a href="ContactP/contact.php">Contact</a></li> Pagina is nog niet nodig in sprint 1-->
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <div id="banner">
+      <img src="../img/Screenshot 2024-04-22 083310.png" alt="height-50">
+      <div class="content text-center">
+        <h1>
+        Astronomie
+        </h1>
+        <p>MBO Utrecht</p>
+      </div>
+      </img>
+    </div>
+  </header>
+
+
+  <div>
+
+  <h2> Welkom op de opleiding Astronomie </h2>
+
+
+  <div class="bubble"><p><b> Wat kan je allemaal verwachten op deze opleiding </b> <br><br>   
+    Je ontdekt de grenzeloze mogelijkheden van het heelal met onze uitdagende en boeiende opleiding in Astronomie. <br>Of je nu een gepassioneerde sterrenkijker bent of een toekomstige astronoom in spe, onze opleiding biedt een unieke reis door de wonderen van de kosmos.<br><br>
+
+    Leer de geheimen van de sterren, planeten, en melkwegstelsels ontrafelen terwijl je wordt begeleid door toegewijde docenten<br> en onderzoekers die je zullen inspireren met hun expertise en enthousiasme voor het vakgebied.<br><br>
+    
+    Of je nu droomt van het bestuderen van verre exoplaneten, het onderzoeken van zwarte gaten,<br> of het begrijpen van de oorsprong van het universum, onze opleiding biedt een solide basis voor een boeiende carrière in de astronomie.<br><br>
+    
+    Verken onze website verder en ontdek hoe onze opleiding jou kan helpen om de sterren te bereiken!</p>
+  </div>
+
+  <div class="bubble row">
+      <div class="col-6">
+          <p><b>Overzicht van de Astronomie Opleiding</b> <br><br> Onze Astronomie opleiding biedt een diepgaande verkenning van het universum, van de kleinste deeltjes tot de grootste structuren. Deze boeiende en multidisciplinaire opleiding combineert theoretische studies met praktisch onderzoek en biedt studenten de kans om de mysteries van de kosmos te ontrafelen.</p>
+      </div>
+      <div class="zoom media-item col-6 col-l-6 col-m-6 col-s-12 col-xs-12">
+        <img src="" alt="">
+    </div>
+  </div>
+
+  <div class="bubble row">
+      <div class="col-6">
+          <img src="" alt="">
+      </div>
+      <div class="zoom media-item col-6 col-l-6 col-m-6 col-s-12 col-xs-12">
+        <p><b>Cursusonderwerpen:</b> <br><b>-</b> Inleiding tot de Astronomie: Verkenning van de basisprincipes van de astronomie, inclusief de geschiedenis van de astronomie, observatietechnieken en de structuur van het universum.<br><br><b>-</b> Sterrenkunde: Bestudering van de levenscyclus van sterren, stellaire evolutie, en de vorming en eigenschappen van sterrenstelsels.<br><br>
+          <b>-</b> Planetaire Wetenschappen: Onderzoek naar de eigenschappen van planeten, manen, en andere hemellichamen in ons zonnestelsel en daarbuiten. <br><br> <b>En nog meer leuke cursussen!</b></p>
+      </div>
+  </div>
+
+  <div class="bubble">
+      <p><b> Doelen van de Opleiding: </b> <br><br> <b>-</b> Studenten voorbereiden op een loopbaan in de academische wereld, onderzoeksinstituten, de ruimtevaartindustrie, en andere gebieden waarin kennis van de astronomie en ruimtewetenschappen wordt gewaardeerd.<br><br>
+          <b>-</b> Ontwikkelen van analytische en probleemoplossende vaardigheden die relevant zijn voor een breed scala aan beroepen.<br><br>
+          <b>-</b> Stimuleren van een dieper begrip en waardering voor het universum en onze plaats daarin.</p>
+  </div>
+
+  <div class="bubble">
+      <div class="container row">
+          <div class="col-6 col-l-6 col-m-6 col-s-12 col-xs-12">
+              <p> <b>Waarom Astronomie studeren:</b> <br><br> <b>-</b> Ontdek de schoonheid en complexiteit van de school en draag bij aan ons begrip van de kosmos.<br><br>
+                  <b>-</b> Word lid van een gemeenschap van nieuwsgierige en gepassioneerde individuen die gedreven worden door een gezamenlijke interesse in de astronomie.<br><br>
+                  <b>-</b> Bereid je voor op een carrière die zich uitstrekt tot de grenzen van de ruimte en verder, en maak deel uit van baanbrekende ontdekkingen en ontwikkelingen in de kosmische wetenschappen.</p>
+          </div>
+
+      </div>
+  </div>
+  </div>
+
+<script src="../Main/assets/javascript/script.js"></script>
+<script src="../Main/assets/javascript/activeTab.js"></script>
+<script src="../Main/assets/javascript/progressBar.js"></script>
+</home>
+
+<footer>
+        <div class="contact-info">
+            <p class="adres">Australiëlaan 25, Utrecht, 3526AB</p>
+            <p class="email">astronomie@utrecht.nl</p>
+        </div>
+        <div class="social-media-links">
+            <a href="" class="social-media-link" id="twitter">
+                <img src="media/img/Logo_of_Twitter_wikimedia_commons.svg" alt="">
+            </a>
+            <a href="" class="social-media-link" id="facebook">
+                <img src="media/img/2021_Facebook_icon_wikimedia_commons.svg" alt="">
+            </a>
+            <a href="media/img/YouTube_full-color_icon_(2017)_wikimedia_commons.svg" class="social-media-link" id="youtube">
+                <img src="" alt="">
+            </a>
+        </div>
+    </footer>
+</body>
+</html>
