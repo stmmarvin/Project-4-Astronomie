63aac5a157ad83ff5c72a8ba56a79ea8a89e2789
ThomasTadesse <162581606+ThomasTadesse@users.noreply.github.com>
2024-05-16 10:19:43 +0200

    Update index.php favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Astronomie</title>
   <link rel="stylesheet" href="css/main.css" />
+  <link rel="shortcut icon" href="../media/img/favicon.ico" type="image/x-icon">
  
   
   
