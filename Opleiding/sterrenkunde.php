5697e823427f3e80cbea9da8d09c97c8703ceaba
ThomasTadesse <162581606+ThomasTadesse@users.noreply.github.com>
2024-05-20 11:14:28 +0200

    opleiding pagina update xoxo Thomas

---
+++ Opleiding/sterrenkunde.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sterrenkunde Opleiding</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="shortcut icon" href="../media/img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <!-- Header code -->
+    </header>
+    <main>
+        <section class="opleiding_info">
+            <!-- Informatie over Sterrenkunde opleiding -->
+        </section>
+        <!-- Andere secties indien nodig -->
+    </main>
+    <footer>
+        <!-- Footer code -->
+    </footer>
+    <script src="../Main/assets/javascript/script.js"></script>
+    <script src="../Main/assets/javascript/activeTab.js"></script>
+    <script src="../Main/assets/javascript/progressBar.js"></script>
+</body>
+</html>
