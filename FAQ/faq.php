36a07f41d90cf46561beed33380f298402b3c3e1
ThomasTadesse <162581606+ThomasTadesse@users.noreply.github.com>
2024-05-23 10:24:52 +0200

    Update faq.php css

--- FAQ/faq.php
+++ FAQ/faq.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Astronomie</title>
     <link rel="stylesheet" href="../css/navbar.css" />
-    <link rel="stylesheet" href="../css/faq.css" />
+    <link rel="stylesheet" href="../css/main.css" />
     <link rel="shortcut icon" href="../media/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
