018b1d6816a9184e6ca95479c38a824e834742f4
stmmarvin <marvinakpabot@gmail.com>
2024-05-27 13:33:06 +0200

    Astronomie pagina aangemaakt

---
+++ Astronomie/astronomie.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+
+<header>
+    <nav>
+      <div class="container row jc-center">
+        <div class="main-menu">
+          <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../Astronomie/astronomie">Astronomie</a></li>
+            <li><a href="../Opleiding/opleiding.php">Opleiding</a></li>
+            <li><a href="../FAQ/faq.php">FAQ</a></li>
+            <li><a href="../ContactP/contact.php">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="banner">
+      <img src="..\img\moon.png" alt="Astronomie" class="height-50">
+      <div class="content text-center">
+        <h1>Astronomie</h1>
+        <p>Informatie</p>
+      </div>
+    </div>
+  </header>
+
+
+<body>
+    <main class="container">
+    <section class="opleiding_info">
+      <div class="col-12">
+        <h2><img src="../media/img/galaxy.png">Wat is Astronomie?</h2>
+        <p>
+          Astronomie is de wetenschap die zich bezighoudt met de bestudering van hemellichamen zoals sterren, planeten, kometen, en meer.
+          Astronomen bestuderen de oorsprong, evolutie, en de toekomst van het universum.
+        </p>
+        <p>
+          Je studeert Sterrenkunde wanneer je je wilt richten op alle facetten van het heelal.
+          Je doet uitgebreide kennis op over planeten en hun manen, asteroïden, sterren en andere objecten in het heelal.
+          Daarnaast leer je alles over instrumenten en methoden om het heelal te bestuderen. Met deze studie kun je veel
+          betekenen voor de maatschappij. Zo is medische beeldapparatuur, zoals de MRI-scanner, op sterrenkundige technologie
+          gebaseerd en zijn de grootste zonnepanelen gemaakt van materiaal dat ontwikkeld is om een ruimtetelescoop van energie te voorzien.
+        </p>
+      </div>
+    </section>
+
+    <section class="Astronomie-info">
+        <div class="container">
+            <div class="Astronomie card">
+                    <h3>Wikipedia over Astronomie</h3>
+                    <img src="../img\Wikipedia_logo_nohat_only_wikipedia.png" alt="wikipedia logo">
+                    <p><a href="https://nl.wikipedia.org/wiki/Astronomie">
+                        Lees meer over Astronomie op Wikipedia</p>
+                </a>
+
+                
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+    <div class="rechten">
+      <p>Alle rechten voorbehouden</p>
+      <p>© 2024</p>
+    </div>
+    
+    <div class="footer-column">
+      <h3>Pagina's</h3>
+      <a href="../index.php">Home</a><br>
+      <a href="../Opleiding/opleiding.php">Opleiding</a>
+      <a href="../FAQ/faq.php">FAQ</a>
+      <a href="../ContactP/contact.php">Contact</a>
+    </div>
+    
+    <div class="footer-column">
+      <h3>Contactgegevens</h3>
+      <p>Australiëlaan 25</p>
+      <p>Utrecht 3526AB</p>
+      <p><a href="mailto:astronomie@utrecht.nl">astronomie@utrecht.nl</a></p>
+      <div class="social-media-links">
+        <a href="https://twitter.com/utrecht" class="social-media-link" id="twitter">
+          <img src="../media/img/twitter.png" alt="Twitter">
+        </a>
+        <a href="https://www.facebook.com/utrecht" class="social-media-link" id="facebook">
+          <img src="../media/img/facebook.png" alt="Facebook">
+        </a>
+        <a href="https://www.youtube.com/utrecht" class="social-media-link" id="youtube">
+          <img src="../media/img/youtube.png" alt="YouTube">
+        </a>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
