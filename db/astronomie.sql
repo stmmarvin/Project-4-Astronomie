b199402a38f41df02919c81e9e2d72b8268a580e
stmmarvin <marvinakpabot@gmail.com>
2024-05-30 10:35:44 +0200

    DB opnieuw ingevoegd

--- db/astronomie.sql
+++ db/astronomie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 20 mei 2024 om 13:36
+-- Gegenereerd op: 30 mei 2024 om 08:34
 -- Serverversie: 8.2.0
 -- PHP-versie: 8.2.13
 
@@ -33,9 +33,17 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `Firstname` varchar(100) NOT NULL,
   `Lastname` varchar(100) NOT NULL,
   `Email` varchar(100) NOT NULL,
-  `Question` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Question` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `contact`
+--
+
+INSERT INTO `contact` (`id`, `Firstname`, `Lastname`, `Email`, `Question`) VALUES
+(20, 'Marvin', 'Akpabot', 'marvinakpabot@gmail.com', 'text'),
+(21, 'Marvin', 'Akpabot', 'marvinakpabot@gmail.com', 'test');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
