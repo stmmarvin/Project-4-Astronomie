462ff42d977049f6532656517f3d19c657c93310
ThomasTadesse <162581606+ThomasTadesse@users.noreply.github.com>
2024-06-13 15:04:59 +0200

    inlog from & banner

--- Inloggen/inlog.php
+++ Inloggen/inlog.php
@@ -4,10 +4,11 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Inloggen</title>
+  <title>Astronomie</title>
+  <link rel="stylesheet" href="../css/navbar.css" />
   <link rel="stylesheet" href="../css/main.css" />
-  <link rel="stylesheet" href="../css/navbar.css"/>
   <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
@@ -30,7 +31,7 @@
 
 
     <div id="banner">
-    <img src="../img/ " alt="height-50">
+    <img src="../img/letter.jpg" alt="height-50">
       <div class="content text-center">
         <h1>
           Inschrijven voor de opleiding
@@ -40,54 +41,19 @@
       </img>
     </div>
   </header>
+
+
   <div class="Login">
     <h1>Login</h1>
     <form method="POST" action="create.php">
       <label for="username">gebruikersnaam</label>
-      <input type="username" name="username" id="username"  placeholder="Voer hier je gebruikersnaam in."required><br>
+      <input type="username" name="username" id="username"  placeholder="Voer hier je gebruikersnaam in."required>
       <label for="password">Password</label>
-      <input type="password" name="password" id="password" placeholder="Voer hier je wachtwoord in."required><br>
+      <input type="password" name="password" id="password" placeholder="Voer hier je wachtwoord in."required>
       <input type="submit" value="Login">
-      <a href="register.php">Heb je geen account? Register hier.</a>
+      <a href="register.php">Don't have an account? Register here.</a>
     </form>
-  </div>
-<br>
-  <footer class="footer">
-      <div class="rechten">
-      <p>Alle rechten voorbehouden</p>
-      <p>© 2024</p>
-      </div>
 
-      <div class="footer-column">
-      <h3>Pagina's</h3>
-      <a href="../index.php">Home</a><br>
-      <a href="../Astronomie/astronomie.php">Astronomie</a><br>
-      <a href="../Opleiding/opleiding.php">Opleiding</a><br>
-      <a href="../gamePagina/game.php">Game</a><br>
-      <a href="../FAQ/faq.php">FAQ</a><br>
-      <a href="../ContactP/contact.php">Contact</a><br>
-      <a href="../Inloggen/inlog.php">Inschrijven</a><br>
-    </div>
-
-  
-  <div class="footer-column">
-    <h3>Contactgegevens</h3>
-    <p>Australiëlaan 25</p>
-    <p>Utrecht 3526AB</p>
-    <p><a href="mailto:astronomie@utrecht.nl">astronomie@utrecht.nl</a></p>
-    <div class="social-media-links">
-      <a href="https://twitter.com/utrecht" class="social-media-link" id="twitter">
-        <img src="../img/twitter.png" alt="Twitter">
-      </a>
-      <a href="https://www.facebook.com/utrecht" class="social-media-link" id="facebook">
-        <img src="../img/facebook.png" alt="Facebook">
-      </a>
-      <a href="https://www.youtube.com/utrecht" class="social-media-link" id="youtube">
-        <img src="../img/youtube.png" alt="YouTube">
-      </a>
-    </div>
-  </div>
-</footer>
 </body>
 
 </html>
