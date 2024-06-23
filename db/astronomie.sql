2d3bcef41eebf626cbdabff2e8ae596a42ad3740
stmmarvin <marvinakpabot@gmail.com>
2024-06-24 00:42:51 +0200

    Final commit DONE!

--- db/astronomie.sql
+++ db/astronomie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 23 jun 2024 om 19:26
+-- Gegenereerd op: 23 jun 2024 om 22:42
 -- Serverversie: 8.2.0
 -- PHP-versie: 8.2.13
 
@@ -32,11 +32,19 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `id` tinyint NOT NULL AUTO_INCREMENT,
   `Firstname` varchar(100) NOT NULL,
   `Lastname` varchar(100) NOT NULL,
-  `PhoneNumber` tinyint NOT NULL,
+  `PhoneNumber` int NOT NULL,
   `Email` varchar(100) NOT NULL,
   `Question` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `contact`
+--
+
+INSERT INTO `contact` (`id`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `Question`) VALUES
+(38, 'Marvin', 'Akpabot', 684266564, 'marvinakpabot@gmail.com', 'test'),
+(39, 'Marvin', 'is', 656432167, 'jhjf@gmail.com', 'kjk');
 
 -- --------------------------------------------------------
 
@@ -52,7 +60,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `user`
+--
+
+INSERT INTO `user` (`id`, `Username`, `Email`, `Password`, `date`) VALUES
+(44, 'stmakpabot ', 'marvinakpabot@gmail.com', '$2y$10$XzVjteqbWnm32cu0yDFLCOLScGmXZC8RaCXExIQD2DQo6LGW7m5r2', '2024-06-23 22:05:35'),
+(45, 'hoi', 'hoi@hl.nl', '$2y$10$FtMY3Xht736MgrPNSN2w8OkPWXFD2EGw7WQ/WxitHARZykL.DfP/O', '2024-06-23 23:54:55');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
